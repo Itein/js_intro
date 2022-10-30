1fe2495a22732a4b959602f447a95467071bdef0
Itein <nehotsyk@gmail.com>
2022-10-30 16:24:59 +0200

    1.Volume

---
+++ script.js
@@ -0,0 +1,9 @@
+/*1. Об'єм*/
+
+let dia = prompt("Вкажіть діаметр циліндра");
+let height = prompt("Вкажіть висоту циліндра");
+let radius = dia / 2;
+let volume = radius * radius * Math.PI * height;
+
+console.log(radius);
+console.log(volume);
